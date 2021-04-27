b34d471ce047df92d081381a1d8c4c79a8e59508
Lau <lau@efortunetech.com>
2021-04-27 17:53:09 +0800

    add tixi

--- app/src/main/java/com/github/crayonxiaoxin/wanandroid/ui/tixi/TixiScreen.kt
+++ app/src/main/java/com/github/crayonxiaoxin/wanandroid/ui/tixi/TixiScreen.kt
@@ -6,9 +6,7 @@ import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
@@ -16,13 +14,22 @@ import com.google.accompanist.insets.statusBarsPadding
 
 @Composable
 fun TixiScreen(controller: NavHostController) {
-    Scaffold(topBar = { DefaultTopBar(background = Color.White) }) {
+    Scaffold(topBar = {
+        TixiTopBar(
+            background = MaterialTheme.colors.primary,
+            selectedColor = Color.White
+        )
+    }) {
         Text(text = "tixi")
     }
 }
 
 @Composable
-private fun DefaultTopBar(background: Color = MaterialTheme.colors.background) {
+private fun TixiTopBar(
+    background: Color = MaterialTheme.colors.background,
+    selectedColor: Color = MaterialTheme.colors.primary,
+    unSelectedColor: Color = Color.Gray
+) {
     Column(modifier = Modifier.background(color = background)) {
         TopAppBar(
             modifier = Modifier.statusBarsPadding(),
@@ -33,21 +40,32 @@ private fun DefaultTopBar(background: Color = MaterialTheme.colors.background) {
             val selectedIndex = remember {
                 mutableStateOf(0)
             }
+            // 并不能显示原始background
             TabRow(
                 selectedTabIndex = selectedIndex.value,
                 modifier = Modifier.fillMaxSize(),
-                backgroundColor = Color.White,
-                contentColor = MaterialTheme.colors.primary
+                backgroundColor = background,
+                indicator = {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(color = Color.Transparent)
+                    )
+                }
             ) {
                 Tab(
                     selected = selectedIndex.value == 0,
                     modifier = Modifier.fillMaxHeight(),
+                    unselectedContentColor = unSelectedColor,
+                    selectedContentColor = selectedColor,
                     onClick = { selectedIndex.value = 0 }) {
                     Text(text = "体系")
                 }
                 Tab(
                     selected = selectedIndex.value == 1,
                     modifier = Modifier.fillMaxHeight(),
+                    unselectedContentColor = unSelectedColor,
+                    selectedContentColor = selectedColor,
                     onClick = { selectedIndex.value = 1 }) {
                     Text(text = "导航")
                 }
